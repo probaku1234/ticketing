a3bb8811934ec49a9aadd8bc67d71159a6949979
Yun Seob Lee <fhzotxldj@gmail.com>
2022-10-31 13:44:01 +0900

    mocked stripe client

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -5,7 +5,7 @@ import request from "supertest";
 import jwt from 'jsonwebtoken'
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper.ts')
